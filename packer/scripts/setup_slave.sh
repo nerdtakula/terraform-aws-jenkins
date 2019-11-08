8e6fc50c20b9f04927f5c29051a119b25f55b2c7
tocherg <glenn.tocher@vizexplorer.com>
2019-11-08 20:40:00 +1300

    packer to build our own amis

---
+++ packer/scripts/setup_slave.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+echo "Install common tools"
+sudo dpkg --purge --force-depends ca-certificates-java
+sudo apt-get install -y apt-transport-https ca-certificates ca-certificates-java curl software-properties-common
+
+echo "Install Java JDK 8"
+sudo apt-get remove -y java
+sudo add-apt-repository ppa:openjdk-r/ppa -y
+sudo apt-get update
+sudo apt-get install -y openjdk-8-jdk
+java -version
+
+echo "Install Docker engine"
+sudo apt-get remove docker docker-engine docker.io
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt-get update
+sudo apt-get install -y docker-ce
+sudo usermod -aG docker ubuntu
+sudo service docker start
+
+echo "Install git"
+sudo apt-get install -y git
+
+echo "Setup jenkins user"
+sudo useradd -m -s /bin/bash jenkins
