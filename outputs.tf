a58aa5c88adc0eafe7d7e70bec7a9dcb042acd52
tocherg <glenn.tocher@vizexplorer.com>
2019-11-12 10:35:29 +1300

    tweaking outputs

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,14 @@
-output "public_ip" {
+output "jenkins_public_ip" {
   value       = aws_instance.jenkins_master.public_ip
   description = "IP address of the Jenkins instance"
+}
+
+output "public_ip" {
+  value       = aws_eip.default.public_ip
+  description = "Elastic IP address of the Jenkins instance"
+}
+
+output "public_dns" {
+  value       = aws_eip.default.public_dns
+  description = "Elastic DNS address of the Jenkins instance"
 }
