9fd9bd7c2c68136a78a2e65f8907071c54d1adca
tocherg <glenn.tocher@vizexplorer.com>
2019-11-13 15:43:23 +1300

    make sure we are not assigning a public ip to each slave

--- slaves.tf
+++ slaves.tf
@@ -16,12 +16,13 @@ data "template_file" "user_data_slave" {
  * Jenkins slaves launch configuration
  */
 resource "aws_launch_configuration" "slaves" {
-  name            = "${var.namespace}-${var.stage}-${var.name}-slave-launch-config"
-  image_id        = data.aws_ami.ubuntu.id
-  instance_type   = var.slave_instance_type
-  key_name        = var.ssh_key_pair
-  security_groups = [aws_security_group.jenkins_slaves.id]
-  user_data       = data.template_file.user_data_slave.rendered
+  name                        = "${var.namespace}-${var.stage}-${var.name}-slave-launch-config"
+  image_id                    = data.aws_ami.ubuntu.id
+  instance_type               = var.slave_instance_type
+  key_name                    = var.ssh_key_pair
+  security_groups             = [aws_security_group.jenkins_slaves.id]
+  user_data                   = data.template_file.user_data_slave.rendered
+  associate_public_ip_address = false
 
   root_block_device {
     volume_type           = "gp2"
